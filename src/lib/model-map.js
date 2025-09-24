afd08d2660a517f97fb56077974234eeb723f7eb
NtGdi <134926662+TXT2GAM@users.noreply.github.com>
2025-09-24 20:54:57 +0800

    update

--- src/lib/model-map.js
+++ src/lib/model-map.js
@@ -1,5 +1,75 @@
 const modelMap = {
-  "claude-3-7-sonnet-20250219": {
+//////////////////////////////////////////////////
+  "claude-opus-4-1": {
+    "provider": "anthropic",
+    "name": "claude-opus-4-1-20250805",
+    "model_config_display_name": null,
+    "parameters": {
+      "max_tokens": 64000,
+      "temperature": 1
+    }
+  },
+  "claude-opus-4-1-thinking": {
+    "provider": "anthropic",
+    "name": "claude-opus-4-1-20250805",
+    "model_config_display_name": null,
+    "parameters": {
+      "max_tokens": 64000,
+      "thinking": {
+        "type": "enabled",
+        "budget_tokens": 32000
+      },
+      "temperature": 1
+    }
+  },
+//////////////////////////////////////////////////
+  "claude-opus-4": {
+    "provider": "anthropic",
+    "name": "claude-opus-4-20250514",
+    "model_config_display_name": null,
+    "parameters": {
+      "max_tokens": 64000,
+      "temperature": 1
+    }
+  },
+  "claude-opus-4-thinking": {
+    "provider": "anthropic",
+    "name": "claude-opus-4-20250514",
+    "model_config_display_name": null,
+    "parameters": {
+      "max_tokens": 64000,
+      "thinking": {
+        "type": "enabled",
+        "budget_tokens": 32000
+      },
+      "temperature": 1
+    }
+  },
+//////////////////////////////////////////////////
+  "claude-sonnet-4": {
+    "provider": "anthropic",
+    "name": "claude-sonnet-4-20250514",
+    "model_config_display_name": null,
+    "parameters": {
+      "max_tokens": 64000,
+      "temperature": 1
+    }
+  },
+  "claude-sonnet-4-thinking": {
+    "provider": "anthropic",
+    "name": "claude-sonnet-4-20250514",
+    "model_config_display_name": null,
+    "parameters": {
+      "max_tokens": 64000,
+      "thinking": {
+        "type": "enabled",
+        "budget_tokens": 32000
+      },
+      "temperature": 1
+    }
+  },
+//////////////////////////////////////////////////
+  "claude-3-7-sonnet": {
     "provider": "anthropic",
     "name": "claude-3-7-sonnet-latest",
     "model_config_display_name": null,
@@ -8,7 +78,7 @@ const modelMap = {
       "temperature": 1
     }
   },
-  "claude-3-7-sonnet-20250219-thinking": {
+  "claude-3-7-sonnet-thinking": {
     "provider": "anthropic",
     "name": "claude-3-7-sonnet-latest",
     "model_config_display_name": null,
@@ -21,9 +91,10 @@ const modelMap = {
       "temperature": 1
     }
   },
-  "gemini-2.5-pro-preview-05-06": {
+//////////////////////////////////////////////////
+  "gemini-2.5-pro": {
     "provider": "google",
-    "name": "gemini-2.5-pro-preview-05-06",
+    "name": "gemini-2.5-pro",
     "model_config_display_name": null,
     "parameters": {
       "response_format": null,
@@ -35,21 +106,24 @@ const modelMap = {
       "topK": 40
     }
   },
-  "o4-mini": {
-    "provider": "openai",
-    "name": "o4-mini",
+  "gemini-2.5-flash": {
+    "provider": "google",
+    "name": "gemini-2.5-flash",
     "model_config_display_name": null,
     "parameters": {
-      "response_format": {
-        "type": "text"
-      },
-      "reasoning_effort": "high",
-      "max_completion_tokens": 100000
+      "response_format": null,
+      "candidateCount": 1,
+      "stopSequences": null,
+      "maxOutputTokens": 50000,
+      "temperature": 0,
+      "topP": 0.95,
+      "topK": 40
     }
   },
-  "chatgpt-4o-latest": {
+//////////////////////////////////////////////////
+  "gpt-5": {
     "provider": "openai",
-    "name": "chatgpt-4o-latest",
+    "name": "gpt-5",
     "model_config_display_name": null,
     "parameters": {
       "temperature": 1,
@@ -70,21 +144,8 @@ const modelMap = {
       "response_format": null,
       "top_p": 1
     }
-  },
-  "gpt-4.5-preview": {
-    "provider": "openai",
-    "name": "gpt-4.5-preview",
-    "model_config_display_name": null,
-    "parameters": {
-      "temperature": 1,
-      "seed": 0,
-      "response_format": null,
-      "top_p": 1,
-      "frequency_penalty": 0,
-      "presence_penalty": 0
-    }
   }
-
+//////////////////////////////////////////////////
 }
 
 module.exports = modelMap
